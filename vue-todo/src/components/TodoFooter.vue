071ff21606db04b2d64b999977a3b14b6609bd2e
Lee Joo Hyung <wngud985@gmail.com>
2022-03-24 21:57:11 +0900

    화살표 함수로 변경

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -7,11 +7,11 @@
 <script>
 export default {
   methods: {
-    clearTodo: function() {
-      this.$emit('clearAll');
-    }
-  }
-}
+    clearTodo: () => {
+      this.$emit("clearAll");
+    },
+  },
+};
 </script>
 
 <style scoped>
